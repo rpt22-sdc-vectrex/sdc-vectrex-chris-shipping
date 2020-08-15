c944495126739a5d2ee9ef6f52f56befc9621ea6
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-15 12:03:16 -0700

    Create basic structure for connecting to mongo

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const path = require('path');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 7100;
+const db = mongoose.connection;
 
 const app = express();
 
@@ -12,7 +14,13 @@ app.get('/', (req, res) => {
   res.send('This is a test response message');
 })
 
-app.get('/shipping-api/:productId')
+app.get('/shipping-api/:productId', (req, res) => {
+  mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true});
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+
+  })
+})
 
 app.listen(port, () => {
   console.log(`Shipping server is up and running on port ${port}`);
