f97113b8d5a23497ca5df68c5b0ec3155c670deb
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-15 10:49:53 -0700

    Add static middleware

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const path = require('path');
 const port = process.env.PORT || 7100;
 
 const app = express();
 
+app.use('/static', express(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.send('This is a test response message');
 })
+
+app.get('/shipping-api/:productId')
 
 app.listen(port, () => {
   console.log(`Shipping server is up and running on port ${port}`);
