7b55807cbfb1b0242cf4cf3c5d46d96d3e1fc90c
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-17 12:27:01 -0700

    Add simple logging for api requests

--- routes/api/items.js
+++ routes/api/items.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const Shipping = require('../../models/Shipping');
 
 router.get('/all', (req, res) => {
+  console.log(`Returning all items`);
   Shipping.find()
     .sort({ product_id: 1 })
     .then(items => res.json(items));
+});
+
+router.get('/:productId', (req, res) => {
+  console.log(`Returning item ${req.params.productId}`);
+  Shipping.find({product_id: req.params.productId})
+    .then(item => res.json(item))
 });
 
 
