81d93b59d74668ff3ea0d65affd91e5172687b44
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-17 12:04:03 -0700

    Update file name from shipping to Shipping

--- seed/shipping-seeder.js
+++ seed/shipping-seeder.js
@@ -1,6 +1,6 @@
 //requirements
 const mongoose = require('mongoose');
-const Product = require('../models/shipping'); //schema for this collection
+const Product = require('../models/Shipping'); //schema for this collection
 
 //List of items to add to my dev and test databases - product_id 1-100 match product ids from other team member's dbs. The fake data may not match.
 const products = [
