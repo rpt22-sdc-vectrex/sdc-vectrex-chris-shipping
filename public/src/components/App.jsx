d022345d289a0499dce3327b53900d4304b13aae
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-20 16:23:51 -0700

    Stub out react components

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,3 +1,29 @@
-document.write('Welcome to Shipping!');
+import React, { Component } from 'react';
+import CostToShip from './CostToShip';
+import DeliverTo from './DeliverTo';
+import EstimatedDelivery from './EstimatedDelivery';
+import From from './From';
+import Policies from './Policies';
+import ReadyToShip from './ReadyToShip';
 
-console.log('App Loaded!');
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div classname='shippingBox'>
+        <CostToShip />
+        <DeliverTo />
+        <EstimatedDelivery />
+        <From />
+        <Policies />
+        <ReadyToShip />
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ public/src/components/From.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function From(props) {
+  return (
+    <div>
+      <h1>From</h1>
+    </div>
+  )
+}
+
+export default From;
