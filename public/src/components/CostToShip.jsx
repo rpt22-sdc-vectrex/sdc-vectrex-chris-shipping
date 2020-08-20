dc31b6baa50e7f1324e1106fb34f4642406d4758
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-20 16:23:51 -0700

    Stub out react components

---
+++ public/src/components/CostToShip.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CostToShip(props) {
+  return (
+    <div>
+      <h1>Cost to ship</h1>
+    </div>
+  )
+}
+
+export default CostToShip;
---
+++ public/src/components/EstimatedDelivery.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function EstimatedDelivery(props) {
+  return (
+    <div>
+      <h1>Estimated delivery</h1>
+    </div>
+  )
+}
+export default EstimatedDelivery;
