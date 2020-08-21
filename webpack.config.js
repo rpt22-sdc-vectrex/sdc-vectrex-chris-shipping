271e9421f6e99b740fc971c2abb97356f953dfe1
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-20 18:36:25 -0700

    Change path.resolve to path.join to fix compiling bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'public/src/index.jsx'),
+  entry: path.join(__dirname, 'public/src/index.jsx'),
   output: {
     filename: 'ttreit-shipping.js',
-    path: path.resolve(__dirname, '/public/dist')
+    path: path.join(__dirname, '/public/dist')
   },
   module: {
     rules: [
@@ -12,7 +12,10 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       }
     ]
