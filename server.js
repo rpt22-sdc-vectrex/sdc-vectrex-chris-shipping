cf0026ad94f5cfe16cdd846fcf6df5df68679380
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-24 10:41:30 -0700

    Fix linting errors

--- server.js
+++ server.js
@@ -4,30 +4,27 @@ const path = require('path');
 const items = require('./routes/api/items');
 const routes = require('./routes/general/routes');
 
-//const bodyParser = require('body-parser');
+//  const bodyParser = require('body-parser');
 const port = process.env.PORT || 7100;
 
 const app = express();
 
-//connect to mongo
+//  connect to mongo
 mongoose
   .connect(
     'mongodb://localhost:27017/fecShipping',
-    { useNewUrlParser: true, useUnifiedTopology: true }  //per deprecation warnings when run without these
+    { useNewUrlParser: true, useUnifiedTopology: true }, // per deprecation warnings
   )
-  .then(() => console.log(`Connected to fecShipping DB`))
-  .catch(err => console.log(err));
+  .then(() => console.log('Connected to fecShipping DB'))
+  .catch((err) => console.log(err));
 
-//serve static files
+//  serve static files
 app.use('/static', express(path.join(__dirname, 'public')));
 
-//use routes
+//  use routes
 app.use('/shipping-api/', items);
 app.use('/', routes);
 
-
-
-
 app.listen(port, () => {
   console.log(`Shipping server is up and running on port ${port}`);
-})
+});
