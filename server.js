a8cd471f5f433a4ba3400a123743140ba9b45ae2
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-25 21:00:13 -0700

    Change public to client after dir name change

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
   .catch((err) => console.log(err));
 
 //  serve static files
-app.use('/static', express(path.join(__dirname, 'public')));
+app.use('/static', express(path.join(__dirname, 'client')));
 
 //  use routes
 app.use('/shipping-api/', items);
