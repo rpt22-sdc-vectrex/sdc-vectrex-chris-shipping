a6c6ce6c71014c9de9ffff005cbbcf2d1b4df05e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-25 20:59:58 -0700

    Fix linting errors

--- client/src/components/from.test.jsx
+++ client/src/components/from.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
-import { findByTestAttr } from '../../../test/utils';
+import { findByTestAttr } from '../../../Test/utils';
 import From from './From';
 
 configure({ adapter: new Adapter() });
 
-const setUp = (props={}) => {
+const setUp = (props = {}) => {
   const component = shallow(<From {...props} />);
   return component;
 };
