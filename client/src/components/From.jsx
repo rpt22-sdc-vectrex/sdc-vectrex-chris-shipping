6a7c74ef1c375044a528c6342cc607f940f4a9a4
ttreit <14933277+ttreit@users.noreply.github.com>
2020-08-27 22:24:08 -0700

    Fix linting errors

--- client/src/components/From.jsx
+++ client/src/components/From.jsx
@@ -4,7 +4,12 @@ function From(props) {
   return (
     <div>
       <h1 data-test="fromComponent">From</h1>
-      <h3>{props.city}, {props.state}</h3>
+      <h3>
+        {props.city}
+        ,
+        {' '}
+        {props.state}
+      </h3>
     </div>
   );
 }
