e2780db189b6ace3b7877231543f7f426ba6aabc
ttreit <14933277+ttreit@users.noreply.github.com>
2020-09-03 16:15:21 -0700

    Add route for /:productId

--- routes/general/routes.js
+++ routes/general/routes.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Whoa Nelly! There\'s a problem here!');
+});
+
+router.get('/:productId', (req, res, next) => {
+  const options = {
+    root: path.join(__dirname, '../../client/'),
+  };
+  const fileName = 'index.html';
+  res.sendFile(fileName, options, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
 });
 
 module.exports = router;
