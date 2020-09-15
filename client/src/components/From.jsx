63e7002acfb7033b91c7ff81f0317b48f2c67738
ttreit <14933277+ttreit@users.noreply.github.com>
2020-09-14 17:57:09 -0700

    Create basic styling for all components

--- client/src/components/From.jsx
+++ client/src/components/From.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FromStyle = styled.div`
+  font-size: 13px;
+  font-family: "Graphik Webfont",-apple-system,BlinkMacSystemFont,"Roboto","Droid Sans","Segoe UI","Helvetica",Arial,sans-serif;
+  color: #595959
+`;
+
+const FromDataStyle = styled.div`
+  color: #222222
+  font-family: "Guardian-EgypTT",serif;
+  font-weight: 300;
+  letter-spacing: 0.35px;
+  font-size: 20px;
+  line-height: 32px;
+`;
 
 function From(props) {
   return (
     <div>
-      <h1 data-test="fromComponent">Ships from</h1>
-      <h3>
+      <FromStyle data-test="fromComponent">Ships from</FromStyle>
+      <FromDataStyle>
         {props.city}
         ,
         {' '}
         {props.state}
-      </h3>
+      </FromDataStyle>
     </div>
   );
 }
