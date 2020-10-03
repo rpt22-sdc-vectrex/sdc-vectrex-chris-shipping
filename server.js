49ef3c6f6dba4d45f55188982134f1eb2d369b21
Chris Palka <cpalka87gmail.com>
2020-10-03 15:10:21 -0600

    Implement CRUD functionality via MongoDB/MongooseORM

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
 const itemInfo = require('./routes/api/itemInfo');
 const routes = require('./routes/general/routes');
 
-//  const bodyParser = require('body-parser');
 const port = process.env.PORT || 7100;
 
 const app = express();
@@ -17,13 +18,18 @@ dotenv.config({ path: path.join(__dirname, './.env') });
 mongoose
   .connect(
     process.env.DB_CONNECTION,
-    { useNewUrlParser: true, useUnifiedTopology: true }, // per deprecation warnings
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }, // per deprecation warnings
   )
-  .then(() => console.log('Connected to fecShipping DB on Atlas'))
+  .then(() => console.log('DB Connected!'))
   .catch((err) => console.log(err));
 
 //  Begin Middleware
 app.use(cors());
+app.use(bodyParser.json());
 
 //  serve static files
 app.use(express.static('client'));
