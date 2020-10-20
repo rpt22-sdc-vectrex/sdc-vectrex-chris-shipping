75a19157251a14252d583feb3feadcc3d95e7345
Chris Palka <cpalka87gmail.com>
2020-10-19 22:20:15 -0600

    Create csv generation script and seed 1e7 records in pgsql

--- seed/csvBuilder.js
+++ seed/csvBuilder.js
@@ -1,21 +1,22 @@
 /* eslint-disable no-console */
 /* eslint-disable no-await-in-loop */
 const faker = require('faker');
-const JSONStream = require('JSONStream');
+const csvWriter = require('csv-write-stream');
+
 const moment = require('moment');
 const fs = require('fs');
 
-const objectBuilder = async (amount) => {
-  const transformStream = JSONStream.stringify(false);
+const csvBuilder = async (amount) => {
   const start = moment().format('LTS');
   console.log({ start });
-  const fileWrite = fs.createWriteStream('./mockData3.json');
-  transformStream.pipe(fileWrite);
+  const writer = csvWriter();
+  const fileWrite = fs.createWriteStream('./mockData5.csv');
+  writer.pipe(fileWrite);
   let mockShipping;
   const accepted = 'Accepted';
   const noReturns = 'No Returns';
   (async () => {
-    for (let i = 0; i < amount; i += 1) {
+    for (let i = 1; i <= amount; i += 1) {
       mockShipping = {
         product_id: i,
         estimated_delivery: faker.date.future(),
@@ -28,12 +29,14 @@ const objectBuilder = async (amount) => {
         countries_shipped_to: faker.address.country(),
         return_policy: faker.lorem.sentence(),
       };
-      if (!transformStream.write(mockShipping)) {
+      if (!writer.write(mockShipping)) {
         await new Promise((resolve) => fileWrite.once('drain', resolve));
       }
     }
-    transformStream.end();
+    writer.end();
   })();
 };
 
-exports.objectBuilder = objectBuilder;
+csvBuilder(10000000);
+
+exports.csvBuilder = csvBuilder;
