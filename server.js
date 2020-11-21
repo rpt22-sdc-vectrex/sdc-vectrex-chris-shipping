f4809cac9aa21fe51308d3094f0682e5958178b3
Chris Palka <cpalka87gmail.com>
2020-11-20 22:18:44 -0700

    Correct error with nod_modules folder preventing bundle '

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
+
 const newrelic = require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const React = require('react');
+const { renderToString } = require('react-dom');
 const items = require('./routes/items.js');
 
 const app = express();
@@ -21,6 +24,13 @@ app.use(express.static(path.join(__dirname, '/client/dist')));
 
 //  use routes
 app.use('/', items);
+
+app.get('/serverbundle', (req, res) => {
+  const application = renderToString(
+
+  );
+  return res.send(application);
+});
 
 app.listen(PORT, () => {
   console.log(`Shipping server is up and running on port ${PORT}`);
